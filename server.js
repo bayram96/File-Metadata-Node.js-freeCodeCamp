4a3e0d18bfd3a65adb020238018e8dd8f2fc0dcc
Bayramgeldi <byrammr@gmail.com>
2020-12-02 18:44:56 -0500

    finished the project

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer');
+require('dotenv').config()
+
+var storage = multer.diskStorage({
+    destination: './public/uploads'
+});
+
+const upload = multer({storage: storage});
+
+var app = express();
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({"name": req.file.originalname, "type": req.file.mimetype,"size": req.file.size})
+  //console.log(req.file)
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
